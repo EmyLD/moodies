d468f4d8213c97ba67397815526300d0b1c866f8
Emeline LD <emeline.ledu@meline-digital.fr>
2024-03-12 21:07:27 +0100

    add input form and validation

--- client/src/components/form.tsx
+++ client/src/components/form.tsx
@@ -1,36 +1,29 @@
-import { FormEvent, useRef } from "react"
+import { useForm } from "react-hook-form";
 
 const Form = () => {
-    const usernameRef = useRef<HTMLInputElement>(null);
-    const passwordRef = useRef<HTMLInputElement>(null);
-    const loginData = { username: '', password: '' };
-    const error = document.getElementById('error-form');
+  const { register, handleSubmit, formState } = useForm();
 
-    const handleSubmit = (e: FormEvent) => {
-        e.preventDefault();
-        if (usernameRef.current !== null && passwordRef.current !== null) {
-            if (usernameRef.current.value !== '' || passwordRef.current.value !== '') {
-                loginData.username = usernameRef.current.value;
-                loginData.password = passwordRef.current.value;
-                console.log(loginData);
-
-            } else {
-                error.innerText = 'Error: username or password is null';
-            }
-
-        }
-    }
-    return (
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="username">Username :</label>
-            <input id="username" ref={usernameRef} type="text" autoComplete="username" />
-            <label htmlFor="password">Password :</label>
-            <input id="password" ref={passwordRef} type="password" autoComplete="current-password" />
-            <button type="submit">Submit</button>
-            <div id="error-form"></div>
-        </form>
-    )
-
-}
+  formState.errors ? console.log("coucou") : console.log("hello");
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <label htmlFor="username">Username :</label>
+      <input
+        {...(register("username"), { required: true, minLength: 3 })}
+        id="username"
+        type="text"
+        autoComplete="username"
+      />
+      <label htmlFor="password">Password :</label>
+      <input
+        {...register("password", { required: true, minLength: 8 })}
+        id="password"
+        type="password"
+        autoComplete="current-password"
+      />
+      <button type="submit">Submit</button>
+      <div id="error-form"></div>
+    </form>
+  );
+};
 
 export default Form;
