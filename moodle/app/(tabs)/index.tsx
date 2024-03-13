7114c958cc4c78cae8e0edcc2ba9c34037925bb1
Emeline LD <emeline.ledu@meline-digital.fr>
2024-03-13 18:58:08 +0100

    i dont know sam sam

--- moodle/app/(tabs)/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "tamagui";
-
-export default function LogInTab() {
-  return (
-    <View flex={1} alignItems="center">
-      <Text fontSize={20}>Log in</Text>
-    </View>
-  );
-}
