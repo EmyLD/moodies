2cd3f4e6236d35cac796d5c97e4aa15cce211e98
Emy <145982137+EmyLD@users.noreply.github.com>
2024-03-20 14:18:20 +0100

    push changes to routes

--- backend/app.js
+++ backend/app.js
@@ -1,27 +1,32 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/user');
-const diaryRoutes = require('./routes/diary')
-dotenv = require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/user");
+const diaryRoutes = require("./routes/diary");
+dotenv = require("dotenv").config();
 
-mongoose.connect(process.env.MONGO_URI)
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch((error) => console.log('Connexion à MongoDB échouée !', error));
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch((error) => console.log("Connexion à MongoDB échouée !", error));
 
 const app = express();
-
 
 app.use(express.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
 });
 
-app.use('/api/users', userRoutes);
-app.use('/api/diaries', diaryRoutes)
-
+app.use("/users", userRoutes);
+app.use("/diaries", diaryRoutes);
 
 module.exports = app;
