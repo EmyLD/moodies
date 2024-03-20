7fd919b2a1d9eef35df982d85d163ce229809b16
Emy <145982137+EmyLD@users.noreply.github.com>
2024-03-20 17:30:11 +0100

    user password bcrypt ok

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,6 +1,7 @@
 const User = require("../models/user");
 const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
+const utils = require("../utils/checkPwd");
 
 exports.getAllUsers = (req, res, next) => {
   User.find()
@@ -26,35 +27,57 @@ exports.signup = (req, res, next) => {
 };
 
 exports.login = (req, res, next) => {
-  User.findOne({ email: req.body.email })
-    .then((user) => {
-      if (!user) {
-        return res
-          .status(401)
-          .json({ message: "Paire login/mot de passe incorrecte" });
-      }
-      bcrypt
-        .compare(req.body.password, user.password)
-        .then((valid) => {
-          if (!valid) {
-            return res
-              .status(401)
-              .json({ message: "Paire login/mot de passe incorrecte" });
-          } else {
-            res.status(200).json({
-              userId: user._id,
-              username: user.username,
-              token: jwt.sign(
-                { userId: user._id, username: user.username },
-                "RANDOM_TOKEN_SECRET",
-                { expiresIn: "24h" }
-              ),
-            });
-          }
-        })
-        .catch((error) => res.status(500).json({ error }));
-    })
-    .catch((error) => res.status(500).json({ error }));
+  User.findOne({ email: req.body.email }).then((user) => {
+    if (user == null) {
+      return res.json(`${req.body.email} doesn't exist.`);
+    } else {
+      bcrypt.compare(req.body.password, user.password).then((hash) => {
+        if (hash) {
+          res.status(200).json({
+            userId: user._id,
+            username: user.username,
+            token: jwt.sign(
+              { userId: user._id, username: user.username },
+              "RANDOM_TOKEN_SECRET",
+              { expiresIn: "24h" }
+            ),
+            message: "Mot de passe ok",
+          });
+        } else {
+          res.status(400).json({ message: "Wrong password" });
+        }
+      });
+    }
+  });
+
+  // .then((user) => {
+  //   if (!user) {
+  //     return res
+  //       .status(401)
+  //       .json({ message: "Paire login/mot de passe incorrecte" });
+  //   }
+  //   bcrypt
+  //     .compare(req.body.password, user.password)
+  //     .then((valid) => {
+  //       if (!valid) {
+  //         return res
+  //           .status(401)
+  //           .json({ message: "Paire login/mot de passe incorrecte" });
+  //       } else {
+  //         res.status(200).json({
+  //           userId: user._id,
+  //           username: user.username,
+  //           token: jwt.sign(
+  //             { userId: user._id, username: user.username },
+  //             "RANDOM_TOKEN_SECRET",
+  //             { expiresIn: "24h" }
+  //           ),
+  //         });
+  //       }
+  //     })
+  //     .catch((error) => res.status(500).json({ error }));
+  // })
+  // .catch((error) => res.status(500).json({ error }));
 };
 
 exports.deleteUser = (req, res, next) => {
